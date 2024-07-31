4d87bcfbbe59c95660ac07e047f1feed803bbb18
pipilu0310 <17319389014@163.com>
2024-07-31 23:21:16 +0800

    add workQueue demo

--- client-go-informer/share_informer.go
+++ client-go-informer/share_informer.go
@@ -1,10 +1,12 @@
 package client_go_informer
 
 import (
+	"fmt"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
 )
 
 func ShareInformerDemo() {
@@ -22,15 +24,33 @@ func ShareInformerDemo() {
 	factory := informers.NewSharedInformerFactoryWithOptions(clientSet, 0, informers.WithNamespace("default"))
 	informer := factory.Core().V1().Pods().Informer()
 
+	// 添加一个队列
+	rateLimitingQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			println("add event")
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err != nil {
+				fmt.Printf("get key failed: %v\n", err)
+			}
+			rateLimitingQueue.AddRateLimited(key)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			println("update event")
+			key, err := cache.MetaNamespaceKeyFunc(newObj)
+			if err != nil {
+				fmt.Printf("get key failed: %v\n", err)
+			}
+			rateLimitingQueue.AddRateLimited(key)
 		},
 		DeleteFunc: func(obj interface{}) {
 			println("delete eventls")
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err != nil {
+				fmt.Printf("get key failed: %v\n", err)
+			}
+			rateLimitingQueue.AddRateLimited(key)
 		},
 	})
 
